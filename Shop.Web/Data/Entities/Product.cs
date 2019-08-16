e1dcc6937cfcddbb796f1e8d2c5f6c1821b3abfc
Angel Gonzalezrge <Angel Gonzalez@DESKTOP-F6BQCFSrgggfgfdg>
2019-08-16 19:37:19 -0400

    SeedDB

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -11,6 +11,8 @@ namespace Shop.Web.Data.Entities
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="el campo {0} es obligatorio")]
+        [StringLength(30 , ErrorMessage ="el nombre del producto debe tener un maximo {1} y un minimo de {2} carateres", MinimumLength =4)]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -20,10 +22,10 @@ namespace Shop.Web.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Availabe?")]
         public bool IsAvailabe { get; set; }
